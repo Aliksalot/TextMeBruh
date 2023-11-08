2829ae943caecd3b1657e8af845dabc7efe315e0
Aliksalot <alexkolev05@gmail.com>
2023-11-08 15:57:17 +0200

    init for password handler

---
+++ server/utils/passHandler.js
@@ -0,0 +1,15 @@
+
+// returns encrypted password
+const encryptPassword = (password) => {
+
+}
+
+// boolean -> true if are the same, false otherwise
+const comparePasswords = (password, hashedPassword)=> {
+
+}
+
+module.exports = {
+    comparePasswords,
+    encryptPassword
+}
